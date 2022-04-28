8ab5897f59fa50665f9f08fe97b13aad80bbd91d
ocebr <oceaneb13@hotmail.fr>
2022-04-28 17:26:48 +0200

    data base init

---
+++ database_enrolement/migrations/users_info.sql
@@ -0,0 +1,11 @@
+-- Add migration script here
+create table IF NOT EXISTS users_info(
+    id uuid not null primary key,
+    full_name varchar null,
+    bio varchar null,
+    image varchar null,
+    -- email_verified
+    -- active
+    created_at timestamp not null  default current_timestamp,
+    updated_at timestamp not null  default current_timestamp
+);
